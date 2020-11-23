6d9bb9c04f90eec6d435287fe7d00f67fc1625a7
kempsta <contactkempy@gmail.com>
2020-11-22 22:20:15 -0700

    american py

---
+++ notweb-snippets/hacktheplanet.py
@@ -0,0 +1,16 @@
+# Very dangerous, not for use without parental supervision.
+
+import time
+
+def planethacker():
+    print("Hacking the planet.\n")
+    time.sleep(.500)
+    print("\tPlanet 28% hacked.")
+    time.sleep(.900)
+    print("\tPlanet 61% hacked.")
+    time.sleep(.400)
+    print("\tPlanet 72% hacked.\n")
+    time.sleep(2)
+    print("Planet successfully hacked. (took 3.8 seconds)")
+
+planethacker()
